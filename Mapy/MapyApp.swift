b3719ff48a59dcab5d6e4c79f36ea8a80d85057b
Amish <amish.tufail2002@gmail.com>
2024-04-17 20:10:03 +0500

    Initial Commit

---
+++ Mapy/MapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapyApp.swift
+//  Mapy
+//
+//  Created by Amish on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
